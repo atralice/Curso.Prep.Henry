7cf88dbda8422353557907035c95650b81f89b26
Judith Marlene Simbron <68932877+jude393@users.noreply.github.com>
2020-08-04 22:01:40 -0300

    Homework completo-02JS

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,10 +1,9 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = 'Hola';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
 const nuevoNum = 10;
 
 // Crea una variable booleana:
