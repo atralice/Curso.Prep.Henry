942b54b0679d2104649b3dd260cb2dfe569fc9fc
Judith Marlene Simbron <68932877+jude393@users.noreply.github.com>
2020-08-27 00:27:09 -0300

    JS-VI homework completo

---
+++ 07-JS-VI/homework/JavascriptVI.txt.md
@@ -0,0 +1,5 @@
+JavascriptVI.txt
+
+1. Funciones Callback:
+
+Es una funcion que se pasa a otra funcion como argumento, que luego se invoca dentro de la funcion externa para completar algun tipo de rutina o accion.
--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -2,24 +2,40 @@
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
+  cb();
 }
+  
 
 function sumarArray(numeros, cb) {
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
+  var suma = 0;
+
+  for (var i = 0; i < numeros.length; i++){
+    suma = suma + numeros[i];
+  }
+    cb (suma);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
+  array.forEach(function(elemento, indice) {
+    cb(elemento);
+  });
 }
 
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
+  var nuevoArray = array.map(function(elemento) {
+    return cb(elemento);
+  } ,0);
+  return nuevoArray;
 }
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
