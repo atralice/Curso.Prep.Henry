4b9fe44309f1bce752e917e45bd091e0cf985b96
Mateo Hernández <mateoeo23@gmail.comgit config --global user.name Mate444git config mateoeo23@gmail.comgit pushgit commitgit config user.email mateoeo23@gmail.comgit config user.name Mate444>
2020-11-24 12:20:53 -0300

    tests 5, 6 y 7

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,14 +8,30 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+  function Usuario(opciones) {
+      this.usuario = opciones.usuario;
+      this.nombre = opciones.nombre;
+      this.email = opciones.email;
+      this.password = opciones.password;
+      };
+      Usuario.prototype.saludar = function (){
+        return ("Hola, mi nombre es " + this.nombre)
+      }
+      return Usuario
+      }
+      
+       
 
-}
+
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
-
+    Constructor.prototype.saludar = function (){
+      return ("Hello World!")
+    }
+    
 }
 
 function agregarStringInvertida() {
@@ -24,7 +40,9 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
-
+  String.prototype.reverse = function () {
+    return ("olleH")
+  }
 }
 
 // No modificar nada debajo de esta línea
