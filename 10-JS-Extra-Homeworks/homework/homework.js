e17c17683dec41b02a23db354d734ecaad88c6b7
WandaCirone <wandacirone@gmail.com>
2020-12-08 19:04:51 -0300

    Agrega ejercicio palindromo y test parcial

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -1,5 +1,11 @@
 // No cambies los nombres de las funciones.
 
+function palindromo(frase) {
+    //Definir una función que determine si la cadena de texto que se le pasa como parámetro es un palíndromo, 
+    //es decir, si se lee de la misma forma desde la izquierda y desde la derecha. 
+    //Ejemplo de palíndromo complejo: "La ruta nos aporto otro paso natural" ---> Devuelve true
+    //Tu código:
+  }
 
 
 
@@ -7,5 +13,5 @@
 // --------------------------------
 
 module.exports = {
- 
+    palindromo
 };
--- 10-JS-Extra-Homeworks/homework/tests/JSX.test.js
+++ 10-JS-Extra-Homeworks/homework/tests/JSX.test.js
@@ -1,5 +1,8 @@
 /* eslint-disable no-undef */
 const {
-   
+    palindromo
 } = require('../homework');
 
+describe('palindromo(frase)', function() {
+	it('should return true if the phrase recieved is a palindrome', function() {});
+});
