a5ae1c702166addc1fcadc612158ae01e51fe7f7
Elena Note <elebeatrizgonzalez@gmail.com>
2020-12-09 13:07:27 -0300

    Agrega funciones y sus tests a la homework 10-Extra-Homeworks

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -1,11 +1,69 @@
 // No cambies los nombres de las funciones.
 
 
+// Escribe una función que convierta un objeto en una matriz, donde cada elemento representa 
+//un par clave-valor en forma de matriz.
+//Ejemplo: 
+/*objecto({
+    D: 1,
+    B: 2,
+    C: 3
+  }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
+  
+
+function deObjetoAmatriz(objeto){
+  //Escribe tu código aquí
+}
+
+
+//Realiza una función que reciba como parámetro un string y mueva todas las letras mayúsculas
+// al principio de la palabra.
+//Ejemplo: soyHENRY -> HENRYsoy
+
+function capToFront(s) {
+ //Escribe tu código aquí
+}
+
+
+//Escribe una función, la cual recibe un número y determina si es o no capicúa.
+//La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
+//izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
+
+
+function capicua(numero){
+  //Escribe tu código aquí
+
+}
+
+//Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
+//y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
+
+
+function deleteAbc(cadena){
+  //Escribe tu código aquí
+
+}
+
+
+//Existen dos arrays, cada uno con 5 números. A partir de ello, escribir una función que permita 
+//retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
+//Si no tienen elementos en común, retornar un arreglo vacío.
+//Aclaración: los arreglos no necesariamente tienen la misma longitud
+
+
+function buscoInterseccion(arreglo1, arreglo2){
+  //Escribe tu código aquí
+  
+}
 
 
 // No modificar nada debajo de esta línea
 // --------------------------------
 
 module.exports = {
- 
+   deObjetoAmatriz,
+   capToFront,
+   capicua,
+   deleteAbc,
+   buscoInterseccion
 };
--- 10-JS-Extra-Homeworks/homework/tests/JSX.test.js
+++ 10-JS-Extra-Homeworks/homework/tests/JSX.test.js
@@ -1,5 +1,50 @@
 /* eslint-disable no-undef */
 const {
-   
+    deObjetoAmatriz,
+    capToFront,
+    capicua,
+    deleteAbc,
+    buscoInterseccion
 } = require('../homework');
 
+describe('deObjetoAmatriz(objeto)', function() {
+    it('should return an array, where each element represents a key-value pair in the form of an array.', function() {
+      expect(deObjetoAmatriz({ x: 1, y: 2 })).toEqual([["x" , 1],["y" , 2]]);
+      expect(deObjetoAmatriz({ x: 10, y: 25 })).toEqual([["x" , 10],["y" , 25]]);
+    });
+  });
+
+describe('capToFront(s)', function() {
+    it('should return all capital letters at the beginning of the word.', function() {
+      expect(capToFront("soyHENRY")).toEqual("HENRYsoy");
+      expect(capToFront("DESArrollaDOR")).toEqual("DESADORrrolla");
+    });
+  }); 
+
+describe('capicua(numero)', function() {
+    it('should return Es capicua if the number is read from right to left', function() {
+      expect(capicua(12321)).toEqual("Es capicua");
+      expect(capicua(1111)).toEqual("Es capicua");
+      expect(capicua(105217)).toEqual("No es capicua");
+      expect(capicua(7878700)).toEqual("No es capicua");
+
+    });
+  });  
+
+describe('deleteAbc(cadena)', function() {
+    it('should return the modified string if it has the letters abc or the same string otherwise', function() {
+      expect(deleteAbc("abcefgh")).toEqual("efgh");
+      expect(deleteAbc("abc")).toEqual("");
+      expect(deleteAbc("plural")).toEqual("plurl");
+      expect(deleteAbc("limon")).toEqual("limon");
+
+    });
+  });  
+
+  describe('buscoInterseccion(arreglo1, arreglo2)', function() {
+    it('should return the common elements of the array, and in case they do not match, return an empty array', function() {
+      expect(buscoInterseccion([1, 2 , 3], [1, 5, 8 , 3])).toEqual([1, 3]);
+      expect(buscoInterseccion([7 , 23, 4], [23, 70])).toEqual([23]);
+      expect(buscoInterseccion([1, 20 , 3], [22, 5, 7])).toEqual([]);
+    });
+  });    
