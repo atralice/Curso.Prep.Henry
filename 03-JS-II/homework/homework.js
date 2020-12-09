4160659a87b2c74e2ee41781064ce46b1dda140f
Elena Note <elebeatrizgonzalez@gmail.com>
2020-12-09 20:24:57 -0300

    Modifica ejercicio

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -103,10 +103,7 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  if(numero > 99 && numero < 1000){
-    return true
-  }
-  return false
+  
 }
 
 function doWhile(numero) {
