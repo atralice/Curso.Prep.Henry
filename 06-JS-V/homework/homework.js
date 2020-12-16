f9f8f52967bcf36c30f221ffb9c10333db722eb1
WandaCirone <wandacirone@gmail.com>
2020-12-16 20:31:00 -0300

    Corrige ejercios de Clase Persona y agrega tests

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -24,8 +24,8 @@ function agregarStringInvertida() {
   // Pista: Necesitarás usar "this" dentro de "reverse"
 }
 
-function crearPersona() {
-  //Crea una Clase de ES6 o una función constructor llamada "Persona"
+// ---------------------------------------------------------------------------//
+  //Crea el constructor de la clase "Persona"
   //Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio"
   //Debe tener un método llamado "detalle" que nos devuelve un objeto con las propiedades de la persona y sus valores.
   //Ej: { 
@@ -34,24 +34,25 @@ function crearPersona() {
     //   Edad: 22,
     //   Domicilio: 'Saavedra 123'
     //  }
-  // Devuelve la clase
-  // Tu código:
+
+  class Persona {
+    constructor(/*Escribir los argumentos que recibe el constructor*/) {
+      // Crea el constructor:
+
+    }
 }
-  
+
 function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
 }
   
-function agregarMetodo(Persona) {
+function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
 }
   
-function invocarMetodo() {
-  //Invoca el método recientemente creado para la instancia creada de la clase Persona.
-}
 
 // No modificar nada debajo de esta línea
 // --------------------------------
@@ -60,8 +61,7 @@ module.exports = {
   crearUsuario,
   agregarMetodoPrototype,
   agregarStringInvertida,
-  crearPersona,
   crearInstanciaPersona,
-  agregarMetodo,
-  invocarMetodo
+  agregarMetodo, 
+  Persona
 };
