ed97fef0a119b381f050d93d201270eec3f4fb3e
LenovoUser <julian.her.arg@gmail.com>
2020-12-29 17:36:55 -0300

    first commit homework 02-JS-I

---
+++ 02-JS-I/homework/ejercicio1.md
@@ -0,0 +1,14 @@
+Variables
+Las variables son elementos en el lenguaje de programación que pueden adoptar distintos valores y almacenarlos para utilizarlos más tarde. Por ejemplo si yo defino que una variable 'a' es un número, no podrá adoptar el valor "perro" o "casa", ya que estas últimas son palabras y no números (aunque en JS el valor que puede adoptar una variable puede cambiar, es decir son no tipadas). Si yo defino que el valor de la variable 'a' es el número 4, cada vez que el programa lea 'a' le asignará el número 4.
+
+Strings
+Es un tipo de dato que se caracteriza por ser un bloque de texto. "casa" o "perro" son ejemplos de bloques de texto. Un número (con un valor, como tal) no es en sí mismo texto, pero si pongo ese número entre comillas, el ordenador lo interpretará como texto. "4" es el bloque de texto del número 4. Todos los strings, en lenguajes de programación, van entre comillas. 
+
+Funciones (argumentos, return)
+Las funciones en un lenguaje de programación son un Objeto que puedo invocar para utilizarlo. Algunas ya vienen definidas en el lenguaje de programación, otras las puedo definir yo en base a las variables y argumentos que introduzca en ellas. Los argumentos de una función serán las variables que voy a utilizar en esa función. Van entre parentesis. "Return" es la forma que tengo para que la función me muestre el resultado de su ejecución. Invocar una función la hara trabajar en el programa, pero eso no necesariamente significa que nos mostrará un resultado o un producto de ese trabajo. Para eso debo utilizar "return".
+
+Declaraciones (if)
+Las declaraciones "if" le dicen a la función de ejecutar una determinada secuencia de operaciones si se cumple una determinada condición. "Si sucede esto, ejecutá esto otro", eso es lo que significa el IF para el lenguaje. 
+
+Valores Booleanos (true, false)
+Los valores booleanos provienen de la lógica de Boole. Básicamente son valores que pueden tomar uno de dos: Verdadero o falso. Podría ser también "sí" o "no"; "vacio", "completo"; etc. Lo importante de este tipo de valores es que solo hay dos opciones para ellos, y que son mutuamente excluyentes, es decir que si se adopta el valor de uno, no se puede adoptar el valor del otro. 
--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,22 +1,22 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = Loli;
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 5;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = True;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -28,117 +28,172 @@ const nuevoModulo = 21 % 5 === null;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
+  var str = "Hola";
+  return str;
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
+  var suma = x+y;
+  return suma;
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
+  var resta = x-y;
+  return resta;
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
+  var multiplica = x*y;
+  return multiplica;
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
+  var divide = x/y;
+  return divide;
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (x==y) {
+    return true;
+  }
+  return false;
 }
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (console.log(str1.length) == console.log(str2.length)) {
+    return true;
+    return false;
+  }
 }
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (num<90) {
+    return true;
+  }
+  return false;
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (num>50) {
+    return true;
+  }
+  return false;
 }
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
+  obtenerResto = x%y;
+  return obtenerResto;
 }
 
 function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-}
+  var esPar = num/2;
+  if (esPar == Math.round(esPar)){
+    return true;
+  }
+  return false;
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
+  var esImpar = num/2;
+  if (esImpar !== Math.round(esImpar)){
+    return true;
+  }
+  return false;
 }
 
 function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
+  Math.pow(num,2);
+  return Math.pow();
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
+  Math.pow(num,3);
+  return Math.pow();
 }
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
+  Math.pow(num,exponent);
+  return Math.pow();
 }
 
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
+  var redondearNumero = Math.round(num);
+  return redondearNumero;
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
+  var redondearHaciaArriba = Math.ceil(num);
+  return redondearHaciaArriba;
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  var agregarSimboloExclamacion = str+"!";
+  return agregarSimboloExclamacion;
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
+  var combinarNombres = nombre + " " + apellido;
+  return combinarNombres;
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
+  var obtenerSaludo = "Hola" + nombre + "!";
+  return obtenerSaludo;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
+  var obtenerAreaRectangulo = alto*ancho;
+  return obtenerAreaRectangulo;
 }
 
 
