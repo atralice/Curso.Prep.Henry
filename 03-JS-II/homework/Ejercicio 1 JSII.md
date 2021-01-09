05a6cc6b1eff6742abbaf13805de532d2d92c5e4
LenovoUser <julian.her.arg@gmail.com>
2021-01-09 14:54:37 -0300

    finishes homework 03 JSII

---
+++ 03-JS-II/homework/Ejercicio 1 JSII.md
@@ -0,0 +1,11 @@
+Ejercicio 1 JSII
+
+'FOR'
+For es una expresión que hace que un determinado proceso se ejecute hasta cumplir con una condición. Nosotros determinamos una variable con un valor inicial, una condición, y una variación del valor de la variable. El programa leerá la expresión y, si es true, la ejecutará. Luego de ejecutarla, modificará el valor de nuestra variable en la cantidad que nosotros hayamos estipulado. Así, se seguirá repitiendo siempre que el resultado sea true, y dejará de ejecutarse cuando la respuesta a la condición que habíamos determinado sea false. A este tipo de procesos que se repiten varias veces se los denomina bucles. For es un tipo de bucle. 
+
+
+'&&', '||' y '!'
+Los tres elementos son operadores lógicos. Nos permiten preguntar acerca de la veracidad o falsedad de ciertas condiciones.
+En el caso de '&&' comparamos dos cosas, dos expresiones. Si las dos son verdaderas, el resultado de la expresión será true. Pero si una de las dos, o las dos, son falsas, el resultado de la expresión será false. 
+Para '||', también comparamos dos cosas, pero en este caso basta con que una de las dos expresiones sea verdadera, para garantizar la veracidad de la operación. Si una es verdadera y la otra es falsa, devolverá true; pero si las dos son falsas, devolverá false. 
+'!' significa 'Not'. Nos permite establecer cuando algo NO es. En lineas generales, siempre devolverá lo contrario: por ejemplo, 'Not true' es false.
--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,6 +5,11 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x>y){
+    return x;
+  } else if (x<y){
+    return y;
+  } else return x;
 }
 
 function saludo(idioma) {
@@ -14,18 +19,31 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma==="aleman"){
+    return('Guten Tag!');
+  } else if (idioma==="mandarin"){
+    return('Ni Hao!');
+  } else if (idioma==="ingles"){
+    return('Hello!');
+  } else return('Hola!');
 }
 
 function esDiezOCinco(numero) {
     // Devuelve "true" si "numero" es 10 o 5
     // De lo contrario, devuelve "false"
     // Tu código:
+    if (numero===10 || numero===5){
+      return true;
+    } else return false;
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero<50 && numero>20){
+    return true;
+  }else return false;
 }
 
 function esEntero(numero) {
@@ -36,6 +54,9 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  if (numero===Math.floor(numero)){
+    return true;
+  } else return false;
 }
 
 function fizzBuzz(numero) {
@@ -43,7 +64,11 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-}
+  if ((numero%3===0)&&(numero%5===0)) return('fizzbuzz');
+  else if (numero%3===0) return("fizz");
+  else if (numero%5===0) return('buzz');
+  else return numero;
+} //SE PUEDE HACER TODO CON IF Y UN RETURN AL FINAL
 
 function esPrimo(numero) {
   // Devuelve "true" si "numero" es primo
@@ -51,6 +76,14 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if (numero===2)return true;
+  if(numero===1)return false;
+  if(numero===0)return false;
+  for(let i=2;numero>i;i++){
+    if (numero%i===0){
+      return false;
+    }
+  }return true; 
 }
 
 
