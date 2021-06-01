19a684e2f084e298d61720b601cc40cbf0e263f9
Antonio Tralice <atralice@gmail.com>
2021-05-31 21:49:58 -0300

    adds lt.json to ignore

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,10 +1,10 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = 'toni';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 12;
 
 // Crea una variable booleana:
 const nuevoBool = null;
