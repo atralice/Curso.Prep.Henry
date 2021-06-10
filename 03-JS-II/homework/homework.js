ac618b3f0875518a892262133e54f54bb9f685b6
Alexandre Texeira Bondelas <69311016+atexeira@users.noreply.github.com>
2021-06-10 01:24:41 -0300

    update esPrimo
    otra versión posible para esPrimo(numero)

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -144,17 +144,10 @@ function esPrimo(numero) {
   // Devuelve "true" si "numero" es primo
   // De lo contrario devuelve "falso"
   // Pista: un número primo solo es divisible por sí mismo y por 1
-  // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
   // Test
-  if( numero < 2) return false;
-  if(numero === 2) return true;
-  for(var i = 2; i < numero; i++) {
-    if(numero % i === 0) {
-      return false;
-    }
-  }
-  return true;
+    return numero / numero === 0 && numero % 1 === 0;
+
 }
 
 function esVerdadero(valor){
