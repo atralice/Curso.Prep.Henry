9b968d3a0ef204810f411acb618036de0346bbf9
yael1982 <86452936+yael1982@users.noreply.github.com>
2021-06-25 14:09:29 -0300

    Update JSI.test.js

--- 02-JS-I/homework/tests/JSI.test.js
+++ 02-JS-I/homework/tests/JSI.test.js
@@ -279,11 +279,15 @@ describe('deEuroAdolar(euro)', function() {
 });
 
 describe('esVocal(letra)', function() {
-  it('Deberia devolver el strings "Dato incorrecto", si el valor ingresado tiene más de una letra', function() {
+  it('Deberia devolver el string "Dato incorrecto", si el valor ingresado tiene más de una letra', function() {
     expect(esVocal("la")).toBe("Dato incorrecto");
     expect(esVocal("pero")).toBe("Dato incorrecto");
   });
-  it('Deberia devolver el strings "Es vocal", si el valor ingresado lo es', function() {
+  it('Deberia devolver el string "Dato incorrecto", si el valor ingresado no es vocal', function() {
+    expect(esVocal("l")).toBe("Dato incorrecto");
+    expect(esVocal("p")).toBe("Dato incorrecto");
+  });
+  it('Deberia devolver el string "Es vocal", si el valor ingresado lo es', function() {
     expect(esVocal('a')).toBe('Es vocal');
     expect(esVocal('e')).toBe('Es vocal');
     expect(esVocal('i')).toBe('Es vocal');
