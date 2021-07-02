b718c3f25f7e38770a150a552287b53f2b3458b8
Toni Tralice <atralice@gmail.com>
2021-07-02 09:19:39 -0300

    Update homework.js

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -51,8 +51,9 @@ function tieneEmail(usuario) {
 }
 
 
+
 function tienePropiedad(objeto, propiedad) {
-  // Devuelve "true" si el objeto tiene el valor del argumento "propiedad"
+  // Devuelve "true" si el objeto (parámetro "objeto") tiene una propiedad (key) cuyo nombre es igual al valor del argumento "propiedad"
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
