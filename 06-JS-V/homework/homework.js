f4b086a308195ef79a0fb68ba44227aba8a440ec
Kaiser-bit <andres.utengo@correounivalle.edu.co>
2021-09-21 21:08:11 -0500

    Se completa la funcion crearUsuario().

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,6 +8,15 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+
+  function Usuario(opciones) {
+    this.usuario = opciones["usuario"];
+    this.nombre = opciones["nombre"];
+    this.email = opciones["email"];
+    this.password = opciones["password"];  
+  }
+  
+  Usuario.prototype.saludar = function() { return 'Hola, mi nombre es ' + this.nombre; }
 }
 
 function agregarMetodoPrototype(Constructor) {
