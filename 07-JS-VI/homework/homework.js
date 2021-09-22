107d4fc6c310f631bb25f667317da9f66daa1ffa
Kaiser-bit <andres.utengo@correounivalle.edu.co>
2021-09-22 00:18:10 -0500

    Se da solucion la homework del capitulo VI.

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,20 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  return nombre.replace(nombre.charAt(0),nombre.charAt(0).toUpperCase());
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1,n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +25,21 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  const suma = numeros.reduce(function(acc, elemento){
+    if (Number.isInteger(elemento)) {
+      return acc + elemento;
+    }
+  });
+  cb(suma);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(element => {
+    cb(element);
+  });
 }
 
 function map(array, cb) {
@@ -35,12 +47,20 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  return array.map(element => {return cb(element);});
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  nuevoArreglo = [];
+  array.forEach(element => {
+    if (element.charAt(0) === 'a') {
+      nuevoArreglo.push(element);
+    }
+  });
+  return nuevoArreglo;
 }
 
 // No modificar nada debajo de esta línea
