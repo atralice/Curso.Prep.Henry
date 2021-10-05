dba41b157384c86d68376c06debffd1f0e1be987
jocrme <84425553+jocrme@users.noreply.github.com>
2021-10-05 03:10:39 -0500

    tarea 1 Curso.pre Henry
    estos son los ejercicios realizados para  curso pre. henry  unidad manejo de variables, condicional y strings

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,6 +1,10 @@
+
+// realizado por johann cruz
+
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
+<<<<<<< Updated upstream
 const nuevaString = 'Hola';
 
 // Crea una variable numérica, puede ser cualquier número:
@@ -8,10 +12,20 @@ const nuevoNum = 8;
 
 // Crea una variable booleana:
 const nuevoBool = true;
+=======
+const nuevaString = "hola"
+
+// Crea una variable numérica, puede ser cualquier número:
+const nuevoNum = 10;
+
+// Crea una variable booleana:
+const nuevoBool = true
+>>>>>>> Stashed changes
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
 
+ 
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * 4 === 40 ;
 
@@ -28,120 +42,257 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
+<<<<<<< Updated upstream
     return str;
+=======
+  return str
+
+
+  
+>>>>>>> Stashed changes
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
+<<<<<<< Updated upstream
   return x + y;
+=======
+ return x + y 
+
+ 
+
+  
+>>>>>>> Stashed changes
 }
 
 function resta(x, y) {
   // Resta "x" de "y" y devuelve el valor
   // Tu código:
+<<<<<<< Updated upstream
   return x - y;
+=======
+   
+ return x - y 
+  
+  
+>>>>>>> Stashed changes
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
+<<<<<<< Updated upstream
   return x * y;
+=======
+
+  return  x * y 
+  
+>>>>>>> Stashed changes
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
+<<<<<<< Updated upstream
   return x / y;
+=======
+  return  x / y
+  
+>>>>>>> Stashed changes
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
+<<<<<<< Updated upstream
   if (x === y) {
     return true;
   } else {
     return false;
   }
+=======
+  if (x == y){
+  return true}
+  else{ return false}
+
+
+  
+>>>>>>> Stashed changes
 }
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
+<<<<<<< Updated upstream
   return str1.length === str2.length;
+=======
+  if(str1.length==str2.length){
+    return true
+  }
+  else {
+   return false
+
+  }
+
+  
+>>>>>>> Stashed changes
 }
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
+<<<<<<< Updated upstream
   if(num < 90) {
     return true;
   } else {
     return false;
   }
+=======
+
+  if(num < 90 ){
+    return true
+  }
+  else {
+   return false
+
+  }
+
+
+
+
+  
+>>>>>>> Stashed changes
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
+<<<<<<< Updated upstream
   return num > 50;
+=======
+  if(num >50){
+    return true
+  }
+  else {
+   return false
+
+  }
+
+
+
+
+  
+>>>>>>> Stashed changes
 }
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
+<<<<<<< Updated upstream
   return x % y;
+=======
+
+  return x%y 
+  
+>>>>>>> Stashed changes
 }
 
 function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
+<<<<<<< Updated upstream
   if( num % 2 === 0) {
     return true;
   } else {
     return false;
   }
+=======
+  
+  if( num % 2 == 0){
+    return true
+  }
+  else {
+   return false
+
+  }
+
+
+
+  
+>>>>>>> Stashed changes
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
+<<<<<<< Updated upstream
   if(num % 2 === 1) {
     return true;
   } else {
     return false;
   }
+=======
+
+  if(num % 2 != 0){
+    return true
+  }
+  else {
+   return false
+
+  }
+
+  
+>>>>>>> Stashed changes
 }
 
 function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
+<<<<<<< Updated upstream
   return Math.pow(num, 2);
+=======
+
+  return num*num
+  
+>>>>>>> Stashed changes
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
+<<<<<<< Updated upstream
   return num * num * num;
+=======
+  return Math.pow(num, 3)
+  
+>>>>>>> Stashed changes
 }
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
+<<<<<<< Updated upstream
   return Math.pow(num, exponent);
+=======
+
+  return Math.pow(num, exponent)
+  
+>>>>>>> Stashed changes
 }
 
 function redondearNumero(num) {
   // Redondea "num" y devuélvelo
   // Tu código:
+<<<<<<< Updated upstream
   // var resto = num % 1;
   // var entero = num - resto;
   // if (resto >= 0.5) {
@@ -150,18 +301,35 @@ function redondearNumero(num) {
   //   return entero;
   // }
   return Math.round(num);
+=======
+ return Math.round(num)
+
+
+  
+>>>>>>> Stashed changes
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba y devuélvelo
   // Tu código:
+<<<<<<< Updated upstream
   return Math.ceil(num);
+=======
+ return Math.ceil(num)
+>>>>>>> Stashed changes
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
+<<<<<<< Updated upstream
   return Math.random();
+=======
+ return Math.random()
+
+
+  
+>>>>>>> Stashed changes
 }
 
 function esPositivo(numero) {
@@ -169,6 +337,10 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
+<<<<<<< Updated upstream
+=======
+  
+>>>>>>> Stashed changes
   if(numero === 0) {
     return false;
   }
@@ -179,6 +351,8 @@ function esPositivo(numero) {
     return "Es negativo";
   }
 }
+
+
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
@@ -191,8 +365,13 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
+<<<<<<< Updated upstream
   var combinado = nombre + ' ' + apellido;
   return combinado;
+=======
+  return  `${nombre} ${apellido}`
+  
+>>>>>>> Stashed changes
 }
 
 function obtenerSaludo(nombre) {
@@ -205,13 +384,24 @@ function obtenerSaludo(nombre) {
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un cuadrado teniendo su altura y ancho
   // Tu código:
+<<<<<<< Updated upstream
   return alto * ancho;
+=======
+  return  alto * ancho
+  
+>>>>>>> Stashed changes
 }
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
+<<<<<<< Updated upstream
   return lado * 4
+=======
+
+  return lado + lado + lado + lado 
+  
+>>>>>>> Stashed changes
 }
 
 function areaDelTriangulo(base, altura){
@@ -219,13 +409,23 @@ function areaDelTriangulo(base, altura){
   //Escribe tu código aquí
   return (base * altura)/2
 
+return (base * altura)/2 
+
+
 }
 
 function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. 
   //Escribe un programa que pida al usuario un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
+<<<<<<< Updated upstream
   return euro * 1.2
+=======
+
+  return euro* 1.20
+
+  
+>>>>>>> Stashed changes
 }
 
 function esVocal(letra){
@@ -240,6 +440,16 @@ function esVocal(letra){
     return "Es vocal"
   }
   return "Dato incorrecto"
+<<<<<<< Updated upstream
+=======
+
+
+
+
+  
+}
+
+>>>>>>> Stashed changes
 
 }
 
