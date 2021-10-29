3e04cf4f3e92b617f5add0d5fd29d2dc79908d44
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-10-29 15:42:43 -0300

    assets build working

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,9 @@
 module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("_src/styles");
-  // eleventyConfig.addPassthroughCopy("_src/assets");
-  eleventyConfig.addPassthroughCopy("logo.png");
-  eleventyConfig.addPassthroughCopy("00-PrimerosPasos/img/");
+  eleventyConfig.addPassthroughCopy("_src/assets");
+  // eleventyConfig.addPassthroughCopy("logo.png");
+  // eleventyConfig.addPassthroughCopy("00-PrimerosPasos/img/");
 
       return {
         dir: {
