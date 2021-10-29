fe0598ff340e17dac5e5697d6fc4143c61636e31
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-10-29 15:26:53 -0300

    initial config

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function (eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy("_src/styles");
+  // eleventyConfig.addPassthroughCopy("_src/assets");
+  eleventyConfig.addPassthroughCopy("logo.png");
+  eleventyConfig.addPassthroughCopy("00-PrimerosPasos/img/");
+
+      return {
+        dir: {
+          includes: "/_src/layouts",
+          data: "/_src/data",
+          output: "_dist",
+        },
+      };
+    };
