d93aa398fd218e28537b770dcb110b9fbb009b45
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-11-01 00:03:50 -0300

    syntaxHighlight config

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,11 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const syntaxHighlight = require ("@11ty/eleventy-plugin-syntaxhighlight");
+
 
 module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   eleventyConfig.addPassthroughCopy("_src/styles");
   eleventyConfig.addPassthroughCopy("_src/assets");
