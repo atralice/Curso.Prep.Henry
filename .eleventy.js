0e20c8c14affc695da26ba1fcdcbd2671e1adbab
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-11-03 16:05:49 -0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,6 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const syntaxHighlight = require ("@11ty/eleventy-plugin-syntaxhighlight");
 
-
-
 module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
