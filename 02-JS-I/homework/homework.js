c25284f98caa5aa56795c5f65808d460d824504c
Emmanuelvp88 <Emmanuel.villalp@outlook.es>
2021-12-24 12:17:16 -0600

    si se puede

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "Hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = "Hola mundo";
 
 // Crea una variable booleana:
 const nuevoBool = null;
