db68568f83fe0209cecf9b458363d5991ce1d3ca
mfbetancourtp <42900005+mfbetancourtp@users.noreply.github.com>
2021-12-28 10:10:57 -0500

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = null;
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 'hola';
 
 // Crea una variable booleana:
 const nuevoBool = null;
