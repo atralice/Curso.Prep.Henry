890f17fd8b885dbf14fcf23124fdf75084207a89
Santibio <87582357+Santibio@users.noreply.github.com>
2022-01-07 16:56:17 -0300

    prueba

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -22,6 +22,7 @@ function agregarPropiedad(objeto, property) {
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
   objeto[property] = null
+  //prueba
   return objeto
 }
 
