972ae3d952c20614864b0054aa811704e8c900fe
Santibio <87582357+Santibio@users.noreply.github.com>
2022-01-10 09:29:07 -0300

    prueba hoy

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -57,8 +57,6 @@ function tieneEmail(usuario) {
 }
 
 
-
-
 function tienePropiedad(objeto, propiedad) {
   // Devuelve "true" si el objeto (parámetro "objeto") tiene una propiedad (key) cuyo nombre es igual al valor del argumento "propiedad"
   // "propiedad" es un string
