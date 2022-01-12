b1c434223e7a94309530926dd97def3eafc170b9
Santibio <87582357+Santibio@users.noreply.github.com>
2022-01-12 19:09:41 -0300

    HW 07 terminada

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -5,27 +5,41 @@ function mayuscula(nombre) {
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
 
+  return nombre[0].toUpperCase() + nombre.slice(1)
 }
+
+
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
-
+  cb()
 }
-// 
+
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
-  //Tu código:
-
+  //Tu código
+  return cb(n1,n2)
 }
+
+
 
 function sumarArray(numeros, cb) {
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+ /*  let suma = 0
+  for(let i = 0; i < numeros.length; i++){
+    suma = suma + numeros[i]
+  } */
+// [1,2,3,4]
+  let suma = numeros.reduce(function(a,v){
+    return a + v
+  })
+  cb(suma)
 
 }
 
@@ -33,6 +47,13 @@ function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  
+  /* for(let i = 0; i < array.length; i++){
+    cb(array[i]);
+  } */
+  array.forEach(function(e){
+    cb(e)
+  })
 
 }
 
@@ -41,15 +62,34 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
- 
+  // [1,2,3]
+
+  /* return array.map(function(e){
+    return cb(e)
+  }); */
+  // [1,2,3,4]
+  let arrayCb = []
+
+  for(let i = 0; i < array.length; i++){
+    arrayCb.push(cb(array[i]));
+  }
+
+  return arrayCb
+
+
 }
+
+
+
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  return array.filter(e=> e[0] === "a")
  
 }
-// asdssss
+
+
 
 
 // No modificar nada debajo de esta línea
