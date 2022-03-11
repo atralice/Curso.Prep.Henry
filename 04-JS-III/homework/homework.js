27c9614cc54c1dcd692245dc0fb0dd0eefb2c276
ccarolina97 <ccarolina_calderon@hotmail.com>
2022-03-10 20:01:53 -0500

    Solución de tareas 2,3 y 4

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,21 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  return array[0];
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array[array.length - 1];
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length;
 }
 
 
@@ -23,6 +26,12 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  var i = 0;
+  var Narray = [];
+  for ( i = 0; i  < array.length; i++) {
+    Narray [i] = array [i]+1;
+  }
+  return Narray;
 }
 
 
@@ -30,6 +39,7 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  return array.unshift(elemento);
 }
 
 
