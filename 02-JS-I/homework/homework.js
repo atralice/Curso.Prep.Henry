f4727e82bf322eaca694fbdef3776b44fbe00ab3
Harbey Baracaldo <molonodad@gmail.com>
2022-05-31 17:09:12 -0500

    Update homework.js
    borrado de linea que genera conflicto

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -68,7 +68,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if()
+  
 }
 
 function menosQueNoventa(num) {
