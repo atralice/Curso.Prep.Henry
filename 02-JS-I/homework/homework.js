e43058150c8f446d361da0dd6904f8b7ccb5ca8b
JTORRESLEAL <juanctc.movies2@gmail.com>
2022-11-21 21:03:25 -0500

    prieba

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
-// Crea una variable "string", puede contener lo que quieras:
-const nuevaString = 'holaduo92';
+// Crea una variable "string", puede conte0ner lo que quieras:
+const nuevaString = 'hauprueba20000';
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
