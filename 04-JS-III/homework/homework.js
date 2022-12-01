88f86084a3be458760b0bf68da1da78c655cb70f
Nicolas Agustin  Martinez <nicoagustinmartinez@hotmail.com>
2022-12-01 20:25:34 -0300

    Update homework.js

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,23 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  return (array[0]);
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return ([array.lenght-1]);
+  
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+return (array.lengt)
+  
 }
 
 
@@ -23,6 +28,8 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+for( i= 0; i >array; i++ )
+return array
 }
 
 
