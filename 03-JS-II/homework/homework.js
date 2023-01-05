c9e9765bb93b3eaa42efc6ec4728a3636b5666cd
Molyyyyy <melody_gutierrez@outlook.com>
2023-01-05 10:29:37 -0300

    second commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -10,7 +10,7 @@ function obtenerMayor(x, y) {
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
-  //Si es menor, devolver --> "Not allowed"
+  //Si es menor, devolver ---> "Not allowed"
 }
   
 function conection(status) {
